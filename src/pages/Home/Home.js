d2969d19bcaa7b107a139fe803f17487c34ba18d
Felipe Campos <felipecampos50123@gmail.com>
2023-07-05 11:37:28 -0300

    Iniciando busca no localStorage (READ)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,26 +1,54 @@
 import React from "react";
 import styles from "./Home.module.css";
-import { IoAddOutline } from "react-icons/io5";
 
+//Hooks
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+//Icons
+import { IoAddOutline } from "react-icons/io5";
+
 const Home = () => {
+  const [fruits, setFruits] = useState(new Array());
+
   const Navigate = useNavigate();
 
   const handleClick = () => {
     Navigate("/cadastrar");
   };
 
+  useEffect(() => {
+    if (localStorage.hasOwnProperty("fruits")) {
+      setFruits(JSON.parse(localStorage.getItem("fruits")));
+    }
+  }, []);
+
   return (
     <div className={styles.home}>
-      <div className={styles.p}>
-        <p>Cadastre sua primeir fruta</p>
-      </div>
-      <div>
-        <button className={styles.button} onClick={handleClick}>
-          <IoAddOutline className={styles.icons} /> <span>Cadastrar fruta</span>
-        </button>
-      </div>
+      {fruits.length === 0 ? (
+        <>
+          <div className={styles.p}>
+            <p>Cadastre sua primeira fruta</p>
+          </div>
+          <div>
+            <button className={styles.button} onClick={handleClick}>
+              <IoAddOutline className={styles.icons} />
+              <span>Cadastrar fruta</span>
+            </button>
+          </div>
+        </>
+      ) : (
+        fruits.map((value, index) => {
+          return (
+            <div key={index}>
+              <p>{index}</p>
+              <p>{value.name}</p>
+              <p>{value.price}</p>
+              <p>{value.amount}</p>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 };
