50ecfa508777c2942642fc34f6d7c4c4badd30a2
Felipe Campos <felipecampos50123@gmail.com>
2023-07-11 17:53:05 -0300

    Mostrando preço sempre com 2 cadas decimais na Home

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -12,17 +12,29 @@ import { ModalConfigContext } from "../../context/ModalConfigContext";
 const Cards = (props) => {
   const { toggleModalConfig } = useContext(ModalConfigContext);
 
+  const price = parseFloat(props.price).toFixed(2);
+
   return (
     <>
       <div className={styles.cardContainer}>
         <p className={styles.name}>{props.name}</p>
         <div>
           <p className={styles.price}>
-            <Cash className={styles.icons} /> R${props.price}
+            <Cash className={styles.icons} /> R${price}
           </p>
           <p className={styles.amount}>{props.amount} em estoque</p>
         </div>
-        <RightContent className={styles.config} onClick={() => toggleModalConfig(props.index, props.name, props.price, props.amount)} />
+        <RightContent
+          className={styles.config}
+          onClick={() =>
+            toggleModalConfig(
+              props.index,
+              props.name,
+              props.price,
+              props.amount
+            )
+          }
+        />
       </div>
     </>
   );
